429e7bc3acdfd84f7cb4ff9eae73fa201368ecc7
politrons <politrons@gmail.com>
2018-05-05 22:56:49 +0200

    documentation

--- src/ArithmeticFunction.hs
+++ src/ArithmeticFunction.hs
@@ -3,17 +3,17 @@ module ArithmeticFunction where
 -- |You have two ways to define functions. With and without lambdas
 --arithmeticFunction number = number * 100
 --The same than the line below
-multiplyFunction = (\number -> number * 100):: Int -> Int
+multiplyFunc = (\number -> number * 100):: Int -> Int
 
-sumFunction = (\number -> number + 1000) :: Int -> Int
+sumFunc = (\number -> number + 1000) :: Int -> Int
 
 subtractFunc = (\number -> \amount -> number - amount ) :: Int -> Int -> Int
 
 -- |combining functions
-responseValue = sumFunction (multiplyFunction (multiplyFunction 5))
+responseValue = sumFunc (multiplyFunc (multiplyFunc 5))
 
 -- | In this function one value it's coming from previous function sumFunction, and the 100
-subtractResponse = subtractFunc (sumFunction 200) 100
+subtractResponse = subtractFunc (sumFunc 200) 100
 
 -- |Running the expression. Using show operator we transform from Int -> String
 numericOutput = putStrLn (show responseValue)
