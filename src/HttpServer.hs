717cd857ccf07e881b98c8ac4c943627077195c1
politrons <politrons@gmail.com>
2018-07-27 00:11:03 +0200

    Add monad module

--- src/HttpServer.hs
+++ src/HttpServer.hs
@@ -4,18 +4,26 @@ import Network
 import Control.Concurrent
 import System.IO
 
-msg = "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n"
+responseMessage = "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n"
+port = 1981 :: PortNumber
 
 myServer = withSocketsDo $ do
-    sock <- listenOn $ PortNumber 5002
-    loop sock
+    socket <- createSocket port
+    print ("Server running in port " ++ show port)
+    service socket
 
-loop sock = do
-   (h,_,_) <- accept sock
-   forkIO $ body h
-   loop sock
-  where
-   body h = do
-       hPutStr h msg
-       hFlush h
-       hClose h
+service :: Socket -> IO()
+service socket = do
+   (handler,hostname,portNumber) <- accept socket --  Accept a connection on a socket created by 'listenOn'
+   print ("Request received by hostname " ++ hostname)
+   forkIO $ createResponse handler -- Running response in a new thread
+   service socket --Recursive call to subscribe again for new request
+
+createSocket :: PortNumber -> IO Socket
+createSocket port = listenOn $ PortNumber port -- $ means like use parenthesis listenOn(PortNumber 5002)
+
+createResponse :: Handle -> IO ()
+createResponse handler = do
+                         hPutStr handler responseMessage -- Write the response in output buffer.
+                         hFlush handler -- Sent data in buffer immediately to the operating system.
+                         hClose handler -- Close handler.
