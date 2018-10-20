8233983d99a96924d388ae2fffda00ee0858e110
politrons <politrons@gmail.com>
2018-10-21 00:06:15 +0200

    Add Kafka producer connector

--- src/features/StateMachine.hs
+++ src/features/StateMachine.hs
@@ -5,6 +5,9 @@ import Data.List.NonEmpty
 import Data.Text          (Text)
 import Text.Printf        (printf)
 
+
+-- | State machine Types
+-- -------------------------------
 {-| Types for the shopping-}
 data Item = Item String Double deriving (Show, Eq)
 data Card = Card String deriving (Show, Eq)
@@ -12,11 +15,11 @@ data Money = Money String deriving (Show, Eq)
 
 {-| State Types for the state machine-}
 data States
-  = EmptyBasket
-  | ItemInBasket (NonEmpty Item)
-  | NoPayment (NonEmpty Item)
-  | PaymentSelected (NonEmpty Item) Card
-  | PaymentConfirmed (NonEmpty Item) Card
+   = EmptyBasket
+  | ItemsInBasket [Item]
+  | NoPayment [Item]
+  | PaymentSelected [Item] Card
+  | PaymentConfirmed [Item] Card
   deriving (Show, Eq)
 
 {-| Actions Types for the state machine, that make the machine change from one state to another-}
@@ -28,62 +31,53 @@ data Actions
   | Confirm
   deriving (Show, Eq)
 
+
+-- | State machine implementation
+-- -------------------------------
 {-| Alias Function for to avoid duplicate types in the function declaration.-}
 type StateMachine state action =  state -> action -> IO state
 
 {-|The function expect to receive two arguments the current state of the machine and the action,
    then it return a new state. -}
-checkout:: StateMachine States Actions
--- First state we dont have items
-checkout EmptyBasket (AddItem item) = return (ItemInBasket (item :| []))
---Possible second state add more items
-checkout (ItemInBasket items) (AddItem item) = return (ItemInBasket (item <| items))
---Possible second state to the checkout
-checkout (ItemInBasket items) Checkout =return (NoPayment items)
---Possible third state the select card to pay
-checkout (NoPayment items) (AddPayment card) = return (PaymentSelected items card)
---Last state confirm card and pay
-checkout (PaymentSelected items card) Confirm = return (PaymentConfirmed items card)
+myBasket:: StateMachine States Actions
+{-| First state we dont have items -}
+myBasket EmptyBasket (AddItem item) = return (ItemsInBasket $ createBasket item)
+{-| Possible second state add more items-}
+myBasket (ItemsInBasket items) (AddItem item) = return (ItemsInBasket $ addItem item items)
+{-|Possible second state to the checkout-}
+myBasket (ItemsInBasket items) Checkout = return (NoPayment items)
+{-|Possible third state the select card to pay-}
+myBasket (NoPayment items) (AddPayment card) = return (PaymentSelected items card)
+{-|Last state confirm card and pay-}
+myBasket (PaymentSelected items card) Confirm = return (PaymentConfirmed items card)
+{-|Unhandled state-}
+myBasket state _ = return state
 
---Unhandled state
-checkout state _ = return state
+-- | Utils functions
+-- -------------------------------
+createBasket :: Item -> [Item]
+createBasket item = item : []
 
---To run the state machine
-runFsm :: Foldable f => StateMachine state event -> state -> f event -> IO state
-runFsm = foldM
+addItem :: Item -> [Item] -> [Item]
+addItem item items = item : items
 
---Function to print the state machine
-withLogging :: (Show state, Show event )=> StateMachine state event  -> StateMachine state event
-withLogging stateMachine state event = do newState <- stateMachine state event
-                                          printf "- %s × %s → %s\n" (show state) (show event) (show newState)
-                                          return newState
+-- | Runner
+-- -----------
 runStateMachine :: IO()
-runStateMachine = do state <- runFsm
-                               (withLogging checkout)
-                               EmptyBasket
-                               [ AddItem (Item "potatoes" 23.95)
-                               , AddItem (Item "fish" 168.50)
-                               , Checkout
-                               , AddPayment (Card "0000-0000-0000-0000")
-                               , Confirm
-                               ]
-                     print state
+runStateMachine = do state <- myBasket EmptyBasket $ AddItem (Item "Coca-cola" 1.95)
+                     print ("State:"++ show state)
+                     state <- myBasket state $ AddItem (Item "Twix" 1.50)
+                     print ("State:"++ show state)
+                     state <- myBasket state $ AddItem (Item "Pizza" 9.50)
+                     print ("State:"++ show state)
+                     state <- myBasket state Checkout
+                     print ("State:"++ show state)
+                     state <- myBasket state $ AddPayment (Card "0000-0000-0000-0000")
+                     print ("State:"++ show state)
+                     state <- myBasket state Confirm
+                     print ("State:"++ show state)
 
 
-data State = S0 | S1 | S2
-
-accepts :: State -> String -> Bool
-accepts S0 ('a':xs) = accepts S1 xs
-accepts S1 ('b':xs) = accepts S2 xs
-accepts S2 ('c':xs) = True
-accepts S2 _ = False
-
-decide :: String -> Bool
-decide = accepts S0
 
 
-stateMachine :: IO()
-stateMachine = print $ decide "abc"
 
-stateMachineFalse :: IO()
-stateMachineFalse = print $ decide "bca"
