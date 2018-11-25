536b33e4ed8d8b7b7986755161fc00e29c5f3fcf
politrons <politrons@gmail.com>
2018-11-25 10:38:50 +0100

    Add remove operator

--- src/features/EventSourcingPattern.hs
+++ src/features/EventSourcingPattern.hs
@@ -4,13 +4,17 @@ module EventSourcingPattern where
 
 import Data.Text.Lazy (Text,pack)
 
+type Product = String
+type Price = Double
+
 {-| To define this example we will use the typical grocery basket to be rehydrate -}
-data Basket = Basket{products::[Text], totalPrice::Double, totalDiscount::Double} deriving (Show, Eq)
+data Basket = Basket{products::[Product], totalPrice::Double, totalDiscount::Double} deriving (Show, Eq)
 
 {-| Data type to define all possible events-}
 data Event =
    BasketCreated{ basket:: Basket}
   | ProductAdded{ product::String, price::Double}
+  | ProductRemoved{ product::String, price::Double}
   | DiscountAdded{ discount::Double }
   deriving (Show, Eq)
 
@@ -25,24 +29,39 @@ createBasketCommand = return $ BasketCreated $ Basket [] 0 0
 addProductCommand :: Basket -> String -> Double -> IO Event
 addProductCommand basket product price = return $ ProductAdded product price
 
+{-| Command function apply the command and create event [ProductRemoved] to be rehydrate later on-}
+removeProductCommand :: Basket -> String -> Double -> IO Event
+removeProductCommand basket product price = return $ ProductRemoved product price
+
 {-| Command function apply the command and create event [DiscountAdded] to be rehydrate later on-}
 addDiscountCommand :: Basket -> Double -> IO Event
 addDiscountCommand basket discount = return $ DiscountAdded discount
 
 {-|  EVENTS    -}
 {-| ------------}
-{-| Declaration function for all Events implementations-}
+{-| Declarative function for all Events implementations-}
 applyEvent :: Basket -> Event -> Basket
 
 {-| Pure function to create an empty basket-}
 applyEvent basket (BasketCreated _basket) = basket
 
-{-| Pure function to a add a product in basket-}
-applyEvent basket (ProductAdded product price)= Basket (products basket ++ [pack product]) (totalPrice basket + price) (totalDiscount basket)
+{-| Pure function to create a new Basket adding a product in basket-}
+applyEvent basket (ProductAdded product price)= Basket (products basket ++ [product]) (totalPrice basket + price) (totalDiscount basket)
 
-{-| Pure function to a add a discount in basket-}
+{-| Pure function to create a new Basket adding a discount in basket-}
 applyEvent basket (DiscountAdded newDiscount) = Basket (products basket) (totalPrice basket - newDiscount) ((totalDiscount basket) + newDiscount )
 
+{-| Pure function to create a new Basket removing the product and price from previous basket-}
+applyEvent basket (ProductRemoved product price) = Basket (removeProductFromBasket product (products basket)) (reducePrice basket price) (totalDiscount basket)
+
+{-| Utils  -}
+{-| --------}
+{-| Function to filter from the list of products the one we want to remove-}
+removeProductFromBasket :: Product -> [Product] -> [Product]
+removeProductFromBasket productToRemove products = filter (\product -> product /= productToRemove) products
+
+reducePrice :: Basket -> Price -> Price
+reducePrice basket price = totalPrice basket - price
 
 {-|  PERSISTENCE/REHYDRATE -}
 {-| ------------------------}
@@ -75,6 +94,10 @@ persistEvents = do print "############### PERSISTANCE COMMANDS #################
                    event <- addProductCommand (basket event) "Nachos" 1.20
                    events <- appendEvent events event
                    event <- addDiscountCommand (basket event) 0.20
+                   events <- appendEvent events event
+                   event <- addProductCommand (basket event) "Pepsi" 2.40
+                   events <- appendEvent events event
+                   event <- removeProductCommand (basket event) "Coca-cola" 2.50
                    events <- appendEvent events event
                    print $ show events
                    print "############### REHYDRATE EVENTS #################"
